ec9d176a39c8c0278980eb296234cf201316e89e
Kasia <kasia.bondarava@gmail.com>
2023-02-21 18:12:25 -0500

    Carousel Functionality

---
+++ script.js
@@ -0,0 +1,42 @@
+const imgs = document.getElementById("imgs");
+const prevBtn = document.getElementById("left");
+const nextBtn = document.getElementById("right");
+
+const img = document.querySelectorAll("#imgs img");
+
+let currentSlideIndex = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+    currentSlideIndex++;
+    changeSlides();
+}
+
+function changeSlides() {
+    if (currentSlideIndex > img.length - 1) {
+        currentSlideIndex = 0;
+    } else if (currentSlideIndex < 0) {
+        currentSlideIndex = img.length - 1;
+    }
+
+    imgs.style.transform = `translateX(${-currentSlideIndex * 500}px)`;
+    console.log(currentSlideIndex);
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
+}
+
+prevBtn.addEventListener("click",  () => {
+    currentSlideIndex--;
+    changeSlides();
+    resetInterval();
+});
+
+nextBtn.addEventListener("click", () => {
+    currentSlideIndex++;
+    changeSlides();
+    resetInterval();
+});
